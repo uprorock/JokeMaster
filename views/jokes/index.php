2c310fdb25a9c7403518b6bd7e90119a4030e920
Ilya Bezus <ilya.bezus@gmail.com>
2017-01-18 00:56:09 +0300

    Добавлена функция получения содержимого шутки по ее идентификатору
    Если произошел редирект на главную (шутка не найдена), возвращается
    пустая строка

--- views/jokes/index.php
+++ views/jokes/index.php
@@ -23,4 +23,30 @@ function format ($text) {
 	$result = str_replace ("\r\n\r\n", "<br><br>", $result); 
 	return $result;
 }
+
+// функция получения текста шутки по ссылке
+function get_joke_text($jokeId)
+{
+	$url = "http://bash.im/quote/{$jokeId}";
+
+	$content = file_get_contents($url);
+	
+	$str = trim(preg_replace('/\s+/', ' ', $content)); // supports line breaks inside <title>
+    preg_match("/\<title\>(.*)\<\/title\>/i",$content,$title); // ignore case
+    $page_title = $title[1];	
+
+	if (strpos($page_title, '#') == false) 
+	{
+		// если в заголовке нет # - значит, цитату найти не удалось, возвращаем пустую строку
+		echo 'Not Found';
+		return '';
+	}
+	
+	$first_step = explode( '<div class="text">' , $content );
+	$second_step = explode("</div>" , $first_step[1] );
+	$joke_text = $second_step[0];
+	
+	echo $joke_text;
+	return $joke_text;
+}
 //= LinkPager::widget(['pagination' => $pagination]) ?>
