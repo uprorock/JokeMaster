a3e6af6516bc2af0eafc0400859091a4331af3ba
uprorock <uprorock@ya.ru>
2017-01-19 00:22:41 +0300

    Каждое обновление страницы добавляется по 5 шуток в базу. Выводится 3 случайные шутки при каждом обновлении.

--- controllers/JokesController.php
+++ controllers/JokesController.php
@@ -10,10 +10,19 @@ class JokesController extends Controller
 {
     public function actionIndex()
     {
+		JokesController::AddThreeRandomJokes();
+		
 		//Считаем количество строк в таблице БД
-		$rowCount = Jokes::find()->count();
+		$allRows = Jokes::find()
+					->select('JokeID')
+					->from('Jokes')
+					->all();
+		
+					
+		//print_r($allRows[10]["JokeID"]);
 		// Запрос к БД
-		$randomJokeIDs = JokesController::UniqueRandomNumbersWithinRange(1, $rowCount, 3);//array_rand(range(1,$rowCount), 3);
+		$randomJokeIDs = JokesController::getUniqueIDs($allRows);
+		//print_r($randomJokeIDs);
         $query = Jokes::find()
 			->select('JokeID, JokeText')
 			->from('Jokes')
@@ -28,9 +37,106 @@ class JokesController extends Controller
         ]);
     }
 	
+	private function getUniqueIDs($allRows) {
+		$randomJokeIDs = JokesController::UniqueRandomNumbersWithinRange(1, count($allRows), 3);
+		print_r($randomJokeIDs);
+		//print_r($allRows[$randomJokeIDs[0]]["JokeID"]);
+		$toReturn = array(
+					$allRows[$randomJokeIDs[0]]["JokeID"], 
+					$allRows[$randomJokeIDs[1]]["JokeID"], 
+					$allRows[$randomJokeIDs[2]]["JokeID"]);
+		print_r($toReturn);
+		return $toReturn;
+	}
+	
 	private function UniqueRandomNumbersWithinRange($min, $max, $quantity) {
-    $numbers = range($min, $max);
-    shuffle($numbers);
-    return array_slice($numbers, 0, $quantity);
-}
+		$numbers = range($min, $max-1);
+		shuffle($numbers);
+		print_r(array_slice($numbers, 0, $quantity));
+		return array_slice($numbers, 0, $quantity);
+	}
+	
+	
+	public function AddThreeRandomJokes()
+	{
+		$idsToAdd = JokesController::GetRandomJokesId();
+		foreach ($idsToAdd as &$idToAdd) {
+
+			$bdContainsJoke = true;
+			$jokeExists = false;
+			
+			//echo "\n generated id = ";
+			//echo $idToAdd;
+			
+			$num_rows = Jokes::find()
+				->select('*')
+				->from('Jokes')
+				->where("JokeID='$idToAdd'")
+				->count();
+			
+			if ($num_rows == 0)
+			{
+				$bdContainsJoke = false;
+				$newJokeText = JokesController::GetJokeText($idToAdd);
+				//echo "\n joke = ";
+				//echo "$newJokeText";
+				$jokeExists = ($newJokeText != '') ? true : false;
+			}
+			else
+				$bdContainsJoke = true;	
+
+			if (($bdContainsJoke == false) && ($jokeExists == true))
+			{
+				// запись объекта в бд
+				$model = new Jokes();
+				$model->JokeID = $idToAdd;
+				$data = $str = mb_convert_encoding($newJokeText, 'windows-1252', 'HTML-ENTITIES');
+				$data = $str = mb_convert_encoding($newJokeText, 'windows-1251', 'windows-1252');
+				$data = $str = mb_convert_encoding($newJokeText, 'UTF-8', 'windows-1251');
+				$model->JokeText = $data;
+				$model->save();
+			}	
+		}
+	}
+	
+	// функция получения рандомного id
+	private function GetRandomJokesId()
+	{
+		$url = "http://bash.im/random";
+		$content = file_get_contents($url);
+
+		$result = array();		
+		$first_step = explode('class="id">#' , $content );		
+		for ($i=1; $i<=5; $i++)
+		{
+			$second_step = explode("</a>" , $first_step[$i]);
+			array_push($result, $second_step[0]);
+		}
+
+		return $result;	
+	}
+	
+	// функция получения текста шутки по ссылке
+	private function GetJokeText($jokeId)
+	{
+		$url = "http://bash.im/quote/{$jokeId}";
+		$content = file_get_contents($url);
+	
+		$str = trim(preg_replace('/\s+/', ' ', $content)); // supports line breaks inside <title>
+		preg_match("/\<title\>(.*)\<\/title\>/i",$content,$title); // ignore case
+		$page_title = $title[1];	
+
+		if (strpos($page_title, '#') == false) 
+		{
+			// если в заголовке нет # - значит, цитату найти не удалось, возвращаем пустую строку
+			echo 'Not Found';
+			return '';
+		}
+	
+		$first_step = explode( '<div class="text">' , $content );
+		$second_step = explode("</div>" , $first_step[1] );
+		$joke_text = $second_step[0];
+
+		return $joke_text;
+	}
 }
--- views/jokes/index.php
+++ views/jokes/index.php
@@ -9,18 +9,10 @@ use yii\widgets\LinkPager;
 foreach ($jokes as $joke): 
 ?>
 		<div class="col-lg-4">
-			<h2>Шуточка <?= $joke->JokeID ?></h2>
+			<h2>Шуточка #<?= $joke->JokeID ?></h2>
 
 			<p><?= $joke->JokeText?></p>
 		</div>
 <?php endforeach; ?>
 	</div>
 </div>
-<?
-// Функция замещает символы перехода на новую строку на <br />
-function format ($text) {
-	$result = htmlspecialchars($text);
-	$result = str_replace ("\r\n\r\n", "<br><br>", $result); 
-	return $result;
-}
-//= LinkPager::widget(['pagination' => $pagination]) ?>
